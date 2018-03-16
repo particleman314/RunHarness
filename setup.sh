0c07c2e0446c1d9fcc4408c67e0743f30fa1559c
Mike Klusman <klusman@synopsys.com>
2018-03-16 15:56:08 -0600

    Fixed pathing

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-SLCF_SHELL_TOP="${HOME}/GIT_Projects/ShellLibrary_Synopsys"
+SLCF_SHELL_TOP="$( pwd -L )"
 export SLCF_SHELL_TOP
 
-. ${SLCF_SHELL_TOP}/utilities/common/.define_pather.sh
+. ${SLCF_SHELL_TOP}/utilities/common/define_pather.sh
 . ${SLCF_SHELL_TOP}/utilities/common/program_utilities.sh
